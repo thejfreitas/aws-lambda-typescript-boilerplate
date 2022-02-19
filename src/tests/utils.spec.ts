cdab6f0ce140e4ca892801b641060a17ff33fb7b
J. de Freitas <juniormfreitas@gmail.com>
2022-02-19 12:24:22 -0600

    Remove placeholders

--- src/tests/utils.spec.ts
+++ src/tests/utils.spec.ts
@@ -1,13 +1,5 @@
-import * as utils from '../utils';
-
-describe('Test util functions', () => {
-  describe('getHttpResponseByCode', () => {
-    it('should return "Not Found" if code is 405', () => {
-      expect(utils.getHttpResponseByCode(404)).toEqual('Not Found');
-    });
-
-    it('should return "Method Not Allowed" if code is 405', () => {
-      expect(utils.getHttpResponseByCode(405)).toEqual('Method Not Allowed');
-    });
+describe('Test Environment', () => {
+  it('You should start write your unit tests here', () => {
+    expect(1 + 1).toEqual(2);
   });
 });
