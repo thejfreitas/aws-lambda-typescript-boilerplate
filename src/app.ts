b106d5eca071cb1f013e476c04abfd5890f2a9ec
J. de Freitas <juniormfreitas@gmail.com>
2022-10-01 15:04:47 -0600

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,35 +1,28 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
-import { StatusCode, getStatusPhraseByCode, RequestMethod } from 'ts-http-status-utils';
 
 export const lambdaHandler = async (
   event: APIGatewayProxyEvent,
   context: Context,
 ): Promise<APIGatewayProxyResult> => {
-  try {
-    if (event.httpMethod !== RequestMethod.GET) {
-      return {
-        statusCode: StatusCode.METHOD_NOT_ALLOWED,
-        headers: {},
-        body: JSON.stringify({
-          message: getStatusPhraseByCode(StatusCode.METHOD_NOT_ALLOWED),
-        }),
-      };
-    }
+  let response: APIGatewayProxyResult;
 
-    return {
-      statusCode: StatusCode.OK,
+  try {
+    response = {
       body: JSON.stringify({
-        message: `${getStatusPhraseByCode(StatusCode.OK)} - Ready to start`,
+        message: `Happy coding!`,
       }),
+      statusCode: 200,
     };
   } catch (error) {
-    console.info(error, context.awsRequestId);
-
-    return {
-      statusCode: StatusCode.INTERNAL_SERVER_ERROR,
+    response = {
       body: JSON.stringify({
         message: error instanceof Error ? error.stack : JSON.stringify(error, null, 2),
       }),
+      statusCode: 500,
     };
   }
+
+  console.log('OVER HERE', response);
+
+  return response;
 };
